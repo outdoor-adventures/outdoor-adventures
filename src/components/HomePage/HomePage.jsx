7c9b73b16a1749e435689f0fe4871f8cacd93380
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-05-03 17:27:03 -0500

    add test heading to HomePage component

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,21 +1,18 @@
-import useStore from '../../zustand/store'
-
+import useStore from '../../zustand/store';
 
 function HomePage() {
-  const user = useStore((state) => state.user);
-  const logOut = useStore((state) => state.logOut);
+    const user = useStore((state) => state.user);
+    const logOut = useStore((state) => state.logOut);
 
-  return (
-    <>
-      <h2>Home Page</h2>
-      <p>Your username is: {user.username}</p>
-      <p>Your ID is: {user.id}</p>
-      <button onClick={logOut}>
-        Log Out
-      </button>
-    </>
-  );
+    return (
+        <>
+            <h1>Home Page Test</h1>
+            <h2>Home Page</h2>
+            <p>Your username is: {user.username}</p>
+            <p>Your ID is: {user.id}</p>
+            <button onClick={logOut}>Log Out</button>
+        </>
+    );
 }
-
 
 export default HomePage;
