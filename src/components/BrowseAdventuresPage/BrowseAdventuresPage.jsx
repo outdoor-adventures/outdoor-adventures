177538804f6f62cb204e089fc1c9b1e60de413f8
Johnny De Luna <johndelunaaa@gmail.com>
2025-05-08 18:33:29 -0500

    Added containers for components

--- src/components/BrowseAdventuresPage/BrowseAdventuresPage.jsx
+++ src/components/BrowseAdventuresPage/BrowseAdventuresPage.jsx
@@ -1,10 +1,32 @@
+
+
 import React from 'react'
 
 export default function BrowseAdventuresPage() {
   return (
     <>
     <div>BrowseAdventuresPage</div>
-    <p>Johnnys Pull Request</p>
+
+
+    <div className='navbar'>
+      NavBar Container
+    </div>
+
+    <div className='search-container'>
+      Search / Filters Container
+    </div>
+
+    <div className='map-container'>
+      Map Container
+    </div>
+
+    <div className='adventure-list-container'>
+      Adventure List Container
+    </div>
+
+    <div className='footer'>
+      Footer
+    </div>
     </>
 
   )
