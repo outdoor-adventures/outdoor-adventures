54443c586721d280ca2c5d09d522db2947f1a2af
Johnny De Luna <johndelunaaa@gmail.com>
2025-05-08 18:20:13 -0500

    added containers for BrowseAdventuresPage

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -20,7 +20,7 @@ else {
   pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
-    database: 'prime_app',   // 👈 Change this to the name of your database!
+    database: 'outdoor-adventures',   // 👈 Change this to the name of your database!
   });
 }
 
