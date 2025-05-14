ce6163fc000d40b2a77241f9add8685197a43ed0
Charlotte Ganske <cganske04@icloud.com>
2025-05-14 16:13:16 -0500

    added test map views

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -1,9 +1,10 @@
-import './MapView.css';
-import react from 'react';
-import { useState } from 'react';
-import { Map, Marker } from '@vis.gl/react-google-maps';
 
-//this is for testing. this will be updated when I (charlotte) understand the maps api better.
+import './MapView.css';
+import React from 'react';
+import { useState } from 'react';
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+
+//this is for testing. this will be updated when we have more setup.
 const MapView = () => {
     const [markerLocation, setMarkerLocation] = useState({
         latitude: 51.509865,
@@ -11,18 +12,21 @@ const MapView = () => {
     });
 
     return (
-        <div className='map-container'>
-            <Map
-            style={{ borderRadius: "20px" }}
-            defaultZoom={13}
-            defaultCenter={markerLocation}
-            gestureHandling={"greedy"}
-            disableDefaultUI
-            >
-                <Marker position={markerLocation} />
-            </Map>
-        </div>
+
+        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+            <div className='map-container'>
+                <Map
+                style={{ height: "500px", width: "100% ", borderRadius: "20px" }}
+                defaultZoom={13}
+                defaultCenter={markerLocation}
+                gestureHandling={"greedy"}
+                disableDefaultUI
+                >
+                    <Marker position={markerLocation} />
+                </Map>
+            </div>
+        </APIProvider>
     );
-}
+};
 
 export default MapView;
