c8e3316f11ad89ba6bcedf73f428cabba47ea756
Charlotte Ganske <cganske04@icloud.com>
2025-05-14 18:25:54 -0500

    added sample POST and DELETE to the adventure router

--- server/routes/adventure.router.js
+++ server/routes/adventure.router.js
@@ -16,8 +16,65 @@ router.get('/', (req, res) => {
     .catch ((error) => {
         console.log(`query ${sqlText} failed with error: ${error}`)
         res.sendStatus(500);
-    })
+    });
 }); // END GET
+
+//IN THE WORKS
+//POST ADVENTURE
+router.post('/', (req, res) => {
+    console.log('adventure sent to post is:', req.body);
+    const sqlText = `INSERT INTO "adventures" 
+                    (SQL, COLUMNS, HERE) 
+                    VALUES
+                    ($1, CONTINUING AS MANY AS NEEDED);`;
+
+    const sqlValues = [
+        // VALUES HERE
+    ];
+
+    pool.query(sqlText, sqlValues)
+    
+    .then((result) => {
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log('error in adventure POST', error);
+        res.sendStatus(500);
+    });
+}); //END POST
+
+//GET SINGLE ADVENTURE
+
+// search / filter queries
+
+
+//admin only
+//PUT
+router.put('/:id', (req, res) => {
+    let { id } = req.params;
+    // const { VALUES } = req.body
+    console.log()
+})
+
+//DELETE
+router.delete('/:id', (req, res) => {
+    let { id } = req.params;
+    const sqlText = 'DELETE FROM "adventures" WHERE "id" = $1;';
+
+    pool.query(sqlText, [id])
+
+    .then((result) => {
+        console.log('adventure deleted:', result)
+        res.sendStatus(204);
+    })
+    .catch((error) => {
+        console.log(`error in DELETE ${sqlText}`, error);
+        res.sendStatus(500);
+    });
+}); //END DELETE
+
+
+
 
 
 module.exports = router;
