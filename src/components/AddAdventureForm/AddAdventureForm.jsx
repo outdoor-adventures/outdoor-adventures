eb2287dd5fe96dc33a5b97057988f90ab9b0a777
teairra1990 <craigteairra@gmail.com>
2025-05-14 20:07:21 -0500

    created addAdventureForm with components

---
+++ src/components/AddAdventureForm/AddAdventureForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import useStore from '../../zustand/store';
+
+
+const AddAdventureForm= () => {
+    const [photo, setPhoto] = useState(null);
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
+    const [difficulty, setDifficulty] = useState('');
+    const [location, setLocation] = useState('');
+    const [link, setLink] = useState('');
+    const [description, setDescription] = useState('');
+
+    const submitAdventure = useStore((state) => state.submitAdventure);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submitAdventure({
+      photo,
+      price,
+      category,
+      difficulty,
+      location,
+      link,
+      description,
+    });
+};
+return (
+    <form onSubmit={handleSubmit}>
+      {/* Photo Uploader */}
+      <div>
+        <label htmlFor="photo">Photo:</label>
+        <input
+          type="file"
+          id="photo"
+          accept="image/*"
+          onChange={(e) => setPhoto(e.target.files[0])}
+        />
