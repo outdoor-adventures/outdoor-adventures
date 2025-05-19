79bbc17ddf888007ddf79435efba94587993f3bd
teairra1990 <craigteairra@gmail.com>
2025-05-19 12:41:03 -0500

    add adventures functions

--- src/components/AddAdventureForm/AddAdventureForm.jsx
+++ src/components/AddAdventureForm/AddAdventureForm.jsx
@@ -1,52 +1,14 @@
-import { useState } from 'react';
-import useStore from '../../zustand/store';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-
-const AddAdventureForm= () => {
-    const [photo, setPhoto] = useState(null);
-    const [price, setPrice] = useState('');
-    const [category, setCategory] = useState('');
-    const [difficulty, setDifficulty] = useState('');
-    const [location, setLocation] = useState('');
-    const [link, setLink] = useState('');
-    const [description, setDescription] = useState('');
-
-    const submitAdventure = useStore((state) => state.submitAdventure);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    submitAdventure({
-      photo,
-      price,
-      category,
-      difficulty,
-      location,
-      link,
-      description,
+const AddAdventureForm = () => {
+    const [formData, setFormData] = useState({
+      price: '',
+      category: '',
+      difficulty: '',
+      location: '',
+      link: '',
+      description: '',
+      photo: null,
     });
-};
-return (
-    <form onSubmit={handleSubmit}>
-      {/* Photo Uploader */}
-      <div>
-        <label htmlFor="photo">Photo:</label>
-        <input
-          type="file"
-          id="photo"
-          accept="image/*"
-          onChange={(e) => setPhoto(e.target.files[0])}
-        />
-        </div>
-
-        {/* Adventure Details */}
-        <div>
-            <label htmlFor="photo">Price:</label>
-          
-
-                <label htmlFor="category">Category:</label>
-                <select id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
-
-                </select>
-
-        </div>
-        </form>
+    
