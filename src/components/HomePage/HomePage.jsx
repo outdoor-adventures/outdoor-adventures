17dbc957fd83e544d5ad4c086af4846896d4717d
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-05-25 22:26:27 -0500

    chore(HomePage): remove unused user store import and user info code

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,23 +1,12 @@
-import useStore from '../../zustand/store';
 import HeroSection from '../HeroSection/HeroSection';
 import HowItWorksSection from '../HowItWorksSection/HowItWorksSection';
 import RecentActivitySection from '../RecentActivitySection/RecentActivitySection';
 import Nav from '../Nav/Nav';
 
-function HomePage(props) {
-    const user = useStore((state) => state.user);
-    const logOut = useStore((state) => state.logOut);
-
+function HomePage() {
     return (
         <>
-
-              <Nav pageTitle="Outdoor Adventures" />
-            <h1>Home Page Test - Pat</h1>
-            <h2>Home Page</h2>
-            <p>Your username is: {user.username}</p>
-            <p>Your ID is: {user.id}</p>
-            <button onClick={logOut}>Log Out</button>
-
+            <Nav pageTitle="Outdoor Adventures" />
 
             {/* Home Page Sections (Components) */}
             <HeroSection />
