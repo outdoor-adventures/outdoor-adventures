c5d1aa116fbda31b667186f9b95e7a8140715893
Charlotte Ganske <cganske04@icloud.com>
2025-07-29 16:15:23 -0500

    favorite button now adds favorited adventure to logged in users favorites in database

--- src/components/AddressSearch/FavoriteButton/FavoriteButton.jsx
+++ src/components/AddressSearch/FavoriteButton/FavoriteButton.jsx
@@ -4,15 +4,13 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import axios from 'axios';
 import useStore from '../../../zustand/store'
 
-export default function FavoriteButton({  }) {
-    // const user = useStore((store) => store.user);
+export default function FavoriteButton({ adventureId }) {
+    const user = useStore((store) => store.user);
     const [liked, setLiked] = useState(false);
-    // console.log('adventure is:', adv.id)
-    // console.log('user i is:', user.id);
 
     const toggleLike = async () => {
       try{
-          // await axios.post(`/api/adventures/favorites/${user.id}/${adv.id}`);
+          await axios.post(`/api/adventures/favorites/${user.id}/${adventureId}`);
           setLiked(prev => !prev);
         } catch (err) {
           console.error('Failed to favorite:', err);
