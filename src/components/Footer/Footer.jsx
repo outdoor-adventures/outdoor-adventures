c597b67b2fa83a68b0958a9dfc1406cf9a65a1bf
Johnny De Luna <johndelunaaa@gmail.com>
2025-09-22 16:13:45 -0500

    Added Pending Adventures button to footer (Still needs styling)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,21 @@ import './Footer.css';
 const Footer = () => {
   const user = useStore((store) => store.user);
   const logOut = useStore((state) => state.logOut);
+  const [pendingCount, setPendingCount] = useState(0);
 
-
+  // Fetch pending adventures count when user is admin
+  useEffect(() => {
+    if (user.id && user.user_rank === 1) {
+      fetch('/api/adventures/admin/pending')
+        .then(response => response.json())
+        .then(data => {
+          setPendingCount(data.length);
+        })
+        .catch(error => {
+          console.error('Error fetching pending adventures:', error);
+        });
+    }
+  }, [user.id, user.user_rank]);
 
 
 
@@ -24,36 +37,23 @@ const Footer = () => {
             </li>
         </ul>
         
-      {!user.id ? (
+      {user.id && (
+            <ul>
+              {user.user_rank === 1 && (
+                <li>
+                  <NavLink to="/admin" className="admin-button">
+                    Pending Adventures {pendingCount > 0 && `(${pendingCount})`} 
+                  </NavLink>
+                </li>
+                )}
 
-            <ul className="footer-links">
-            {/* <li> */}
-            
-            {/* <NavLink to="/" className="logout-button">
+                <li>
                 <button onClick={logOut} className="logout_button">Log Out</button>
-            </NavLink> */}
-            
-            {/* </li> */}
+                </li>
             </ul>
-
-      ) : (
-
-
-        <ul>
-
-        <li>
-                <button onClick={logOut} className="logout_button">Log Out</button>
-        </li>
-
-
-
-        </ul>
-
-        
-          )}
-    
-     </div>
-     </div>
+      )}
+      </div>
+    </div>
   );
 };
 
