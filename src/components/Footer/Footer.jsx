9a0c20c23819b01ba200b6777903e6c4520abd77
Johnny De Luna <johndelunaaa@gmail.com>
2025-09-25 09:10:17 -0500

    Remove console logs from Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,11 +9,6 @@ const Footer = () => {
   const logOut = useStore((state) => state.logOut);
   const [pendingCount, setPendingCount] = useState(0);
 
-  console.log('User object:', user);
-console.log('User ID:', user.id);
-console.log('User rank:', user.user_rank);
-console.log('User rank type:', typeof user.user_rank);
-
 
   // Fetch pending adventures count when user is admin
   useEffect(() => {
